7423121da368264af25dd6398a30841d35b55266
Emile <emiletal@gmail.com>
2025-03-14 12:20:02 -0400

    Style recipe grid

--- app/ui/books/recipes-grid.tsx
+++ app/ui/books/recipes-grid.tsx
@@ -1,9 +1,10 @@
 'use client'
 
+import EditIcon from '@mui/icons-material/Edit';
 import Image from "next/image";
 import { LiteRecipe } from "@/app/lib/definitions";
+import RestaurantIcon from '@mui/icons-material/Restaurant';
 import { useRouter } from "next/navigation";
-
 interface Props {
     recipes: LiteRecipe[] | null
 }
@@ -12,19 +13,22 @@ export default function RecipesList({ recipes }: Props) {
     const router = useRouter();
 
     return (
-        <div className="grid grid-cols-6 gap-4">
+        <div className="grid grid-cols-4 gap-8">
             {recipes?.map((recipe) => (
-                <div key={recipe.id} className='p-6 bg-white border border-gray-100 rounded-2xl shadow-md'>
-                    <div className="flex items-center justify-center p-4 w-full h-full">
-                        <Image
+                <div key={recipe.id} className='p-2 bg-white border border-gray-100 rounded-xl shadow-md hover:cursor-pointer' onClick={() => router.push(`/recipe/${recipe.id}`)}>
+                    <div className="flex items-center justify-center w-full h-48 relative">
+                        {recipe.image_url ? (<Image
                             src={recipe.image_url}
                             alt={recipe.title}
-                            width={100}
-                            height={100}
-                            onClick={() => router.push(`/recipe/${recipe.id}`)}
-                            className="hover:cursor-pointer" />
+                            fill
+                            className="object-cover rounded-t-xl max-w-full" />
+                        ) : (
+                            <RestaurantIcon className="w-full h-full text-gray-300" />)}
+                        <div className="absolute top-1 right-1 flex items-center justify-center bg-white rounded-full h-8 min-w-8 hover:cursor-pointer hover:bg-gray-100 group">
+                            <EditIcon className="text-text text-base group-hover:text-lg" />
+                        </div>
                     </div>
-                    <p onClick={() => router.push(`/recipe/${recipe.id}`)} className="hover:cursor-pointer">{recipe.title}</p>
+                    <p className="py-2 text-text">{recipe.title}</p>
                 </div>
             ))}
         </div>
