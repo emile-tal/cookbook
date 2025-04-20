2bd20d6f6b40a5644fa58ce4ac5f2e000795d846
Emile <emiletal@gmail.com>
2025-04-20 13:24:17 -0400

    Add logo to header

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from "next/link";
+import Logo from "./logo";
 import NavLinks from "./nav-links";
 import { SearchBar } from "../components/SearchBar";
 import { Suspense } from "react";
@@ -10,7 +11,10 @@ export default function Header() {
     return (
         <header className="bg-background text-text bg-opacity-85">
             <div className="py-8 md:py-12 flex items-center justify-between container-spacing text-l md:text-xl">
-                <Link href='/'><div>CookBook</div></Link>
+                <Link href='/' className="flex items-center">
+                    <Logo w={50} h={50} />
+                    <span className="text-text font-serif text-2xl">Recipiz</span>
+                </Link>
                 <div className="flex-1 min-w-[300px] max-w-[500px] mx-2">
                     <Suspense fallback={null}>
                         <SearchBar placeholder="Search..." />
